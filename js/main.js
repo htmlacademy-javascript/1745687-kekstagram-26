32d6190027190827a56c10129acedfc8fa99207c
morm934 <morm934@gmail.com>
2022-06-27 10:31:02 +0300

    Module2_task1

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function getIntRandomNumber(min, max) {
+    let absMin = Math.abs(min);
+    let absMax = Math.abs(max);
+  
+    if (absMin > absMax) {
+      [absMax, absMin] = [absMin, absMax];
+    }
+  
+    return Math.floor(Math.random() * (absMax - absMin +  1)) + absMin;
+  }
+  
+  function checkStringLength(string, maxLength) {
+    return string.length <= maxLength;
+  }
+  
+  checkStringLength('строка', 4);
+  
+  getIntRandomNumber(-12,33);
